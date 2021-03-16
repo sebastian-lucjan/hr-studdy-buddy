af812b18d3437b93bbc278a98cd2ff69127f5309
sebastian-lucjan <sebastian.lucjan@gmail.com>
2021-03-16 17:04:42 +0100

    add components: UsersList, UsersListItem

---
+++ src/components/UsersList/UsersLIst.js
@@ -0,0 +1,16 @@
+import users from 'data/users';
+import UsersListItem from 'components/UsersListItem/UsersListItem';
+
+const UsersList = () => {
+  return (
+    <div>
+      <ul>
+        {users.map((userData) => (
+          <UsersListItem userData={userData} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UsersList;
---
+++ src/data/users.js
@@ -0,0 +1,11 @@
+const users = [
+  { name: 'Maria Łucjan', attendance: '12%', average: '2.3' },
+  { name: 'Anna Hoh', attendance: '72%', average: '4.1' },
+  { name: 'Pioter Marek', attendance: '67%', average: '3.8' },
+  { name: 'Sebastian Łucjan', attendance: '100%', average: '4.6' },
+  { name: 'Dorota Gilas', attendance: '48%', average: '2.8' },
+  { name: 'Agatka Gilas', attendance: '70%', average: '3.3' },
+  { name: 'Rodzice Gilas', average: '1.0' },
+];
+
+export default users;
