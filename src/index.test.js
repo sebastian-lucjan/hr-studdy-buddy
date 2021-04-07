cd88d331af84a5639babc97812951387972f4368
sebastian-lucjan <sebastian.lucjan@gmail.com>
2021-04-07 08:58:27 +0200

    Add file: FormField.test.js

---
+++ src/index.test.js
@@ -0,0 +1,16 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+test('Add two values', () => {
+  expect(add(34, 12)).toBe(46);
+  expect(add(34, 12)).not.toBe(1);
+});
+
+const usersList = ['sebastian', 'marysia', 'małe bobo', 'większe bobo'];
+
+it('usersList contains unexpected items', () => {
+  expect(usersList).toContain('sebastian');
+  expect(usersList).not.toContain('apples');
+  // expect(usersList).toContain('xxl bobo');
+});
