53681cb5fc6966123ab4abf12a22c9e7bc329d96
sebastian-lucjan <sebastian.lucjan@gmail.com>
2021-05-26 13:40:23 +0200

    Fix MSW with firefox bug
    Modify index.js and handle worker promise.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import Root from 'views/Root';
 import { worker } from 'mocks/browser';
 
-worker.start();
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+worker.start().then(() => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Root />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+});
